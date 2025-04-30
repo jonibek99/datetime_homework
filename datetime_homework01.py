2d2da3db553ac9200a821eff8a682e24600e02e7
jonibek99 <jonibekannaev5@gmail.com>
2025-04-30 16:18:26 +0500

    time1

--- datetime_homework01.py
+++ datetime_homework01.py
@@ -1,2 +1,5 @@
 #Task 1:
-#Write a Python program that displays the current date and time.
+#Write a Python program that displays the current date and time. 
+from datetime import datetime
+a=datetime.now()
+print(a)
